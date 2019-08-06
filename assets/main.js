8e504f775b6e5cf22ecb94b8bd830479e7fe9214
Bryan Mikaelian <bryan.mikaelian@segment.com>
2019-08-06 11:14:57 -0400

    Sidenav logic

--- assets/main.js
+++ assets/main.js
@@ -1,10 +1,47 @@
-$(function() {
-  $(".js-collapse").on("click", function(e) {
-    e.preventDefault();
-    toggleSideNavSection($(this).parent());
-  });
-});
+/*
+ * Side Nav
+ */
 
-function toggleSideNavSection($el) {
-  $el.toggleClass("expanded");
+// Toggles expanding / collapsing logic
+var nodes = document.querySelectorAll(".js-collapse");
+var path = window.location.pathname;
+
+// Handle trailing slash...
+if (path[path.length - 1] === "/") {
+  path = path.substr(0, window.location.pathname.length - 1);
 }
+
+for (var node of nodes) {
+  node.onclick = function(e) {
+    e.target.parentNode.classList.toggle("expanded");
+  };
+}
+
+// Active link
+var node = document.querySelector(`ul a[href='${path}']`);
+if (node) {
+  node.classList.toggle("active");
+}
+
+// Toggles keeping nested menus open...
+var node = document.querySelector(`.l-sidenav-subnav a[href='${path}']`);
+
+if (node) {
+  var getParent = function(node) {
+    for (; node && node !== document; node = node.parentNode) {
+      if (node.matches(".l-sidenav-collapsible:not(.section)")) return node;
+    }
+
+    return null;
+  };
+
+  var parent = getParent(node);
+  console.log(parent);
+  if (parent) {
+    parent.classList.toggle("expanded");
+  }
+}
+
+/*
+ * Side Nav
+ */
