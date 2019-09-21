c2f4491a248f31f42ecff09de379a6a69ebc3f26
Jeff Knight <jeff.knight@segment.com>
2019-09-20 17:45:52 -0700

    renaming src -> js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './js/index.js',
     output: {
         filename: 'docs.bundle.js',
         path: path.resolve(__dirname, 'assets'),
