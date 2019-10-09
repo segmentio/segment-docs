5a9963c7451d9a36d9bda2dbb1063909f6520080
jedrzejchalubek <jedrzej.chalubek@gmail.com>
2019-10-09 12:14:20 +0200

    feat(): Introduce navbar mobile

--- js/index.js
+++ js/index.js
@@ -3,11 +3,13 @@
 import feedback from './feedback'
 import accordion from './accordion'
 import searchNavbar from './search-navbar'
+import menuMobile from './menu-mobile'
 import headingsAnchors from './headings-anchors'
 import dropdownMenu from './dropdown-menu'
 
 feedback()
 accordion()
 searchNavbar()
+menuMobile()
 headingsAnchors()
 dropdownMenu()
