a6334b15673b391c465af2a6ec66527f8f00860b
Bartłomiej Krakowski <48633090+bartlomiej-krakowski@users.noreply.github.com>
2019-10-10 12:45:04 +0200

    feat(): Introduce accordion component (#106)

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@
 
 import feedback from './feedback'
 import accordion from './accordion'
+import accordionGroup from './accordion-group'
 import searchNavbar from './search-navbar'
 import menuMobile from './menu-mobile'
 import headingsAnchors from './headings-anchors'
@@ -9,6 +10,7 @@ import dropdownMenu from './dropdown-menu'
 
 feedback()
 accordion()
+accordionGroup()
 searchNavbar()
 menuMobile()
 headingsAnchors()
