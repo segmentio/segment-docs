fe9bd543a3609d10f37f115fc760bce6d9502386
Bartłomiej Krakowski <bk@tonik.pl>
2019-10-10 14:33:53 +0200

    fix(): Suport for mobile Safari, add anchor indicator, centering icons

--- js/index.js
+++ js/index.js
@@ -6,6 +6,7 @@ import searchNavbar from './search-navbar'
 import menuMobile from './menu-mobile'
 import headingsAnchors from './headings-anchors'
 import dropdownMenu from './dropdown-menu'
+import anchorsIndicator from './anchors-indicator'
 
 feedback()
 accordion()
@@ -13,3 +14,4 @@ searchNavbar()
 menuMobile()
 headingsAnchors()
 dropdownMenu()
+anchorsIndicator()
