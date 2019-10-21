ec0a04666c85261490e388be616e7ae9201600bd
Bartłomiej Krakowski <bk@tonik.pl>
2019-10-21 16:06:58 +0200

    refactor(): Convert throttle to arrow function

--- js/anchors-indicator/index.js
+++ js/anchors-indicator/index.js
@@ -6,7 +6,7 @@ const options = {
   rootMargin: 50
 }
 
-function throttle(fn, wait) {
+const throttle = (fn, wait) => {
   var time = Date.now();
   return function() {
     if ((time + wait - Date.now()) < 0) {
