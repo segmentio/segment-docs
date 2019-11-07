77f78212509841237e58112ad55e12038be8f94f
Jędrzej Chałubek <jedrzej.chalubek@gmail.com>
2019-11-07 16:21:33 +0100

    fix(plugins): Fix broken character encoding when replacing table cels (#214)

--- _plugins/jekyll-table-markdown-attribute.rb
+++ _plugins/jekyll-table-markdown-attribute.rb
@@ -1,15 +1,3 @@
-require 'jekyll'
-require 'nokogiri'
-
 Jekyll::Hooks.register [:pages, :documents], :pre_render do |post|
-  doc = Nokogiri::HTML.fragment(post.content)
-
-  # Stop if we could't parse with HTML
-  return content unless doc
-
-  doc.css('td').each do |td|
-    td.set_attribute('markdown', 'span')
-  end
-
-  post.content = doc.to_s
+  post.content = post.content.gsub('<td>', '<td markdown="span">')
 end
