805b6771761c2b286d4c4bdd546fca0662599f62
Noeemi <klaudiazawodnik@gmail.com>
2020-05-07 18:14:51 +0200

    feat(): Introduce copy to clipboard function

--- js/headings-anchors/index.js
+++ js/headings-anchors/index.js
@@ -1,6 +1,18 @@
 const COMPONENT_SELECTOR = '[data-headings-anchors]'
 const HEADINGS_SELECTOR = 'h1, h2, h3, h4, h5, h6'
 
+const copyStringToClipboard = (str) => {
+  const el = document.createElement('textarea')
+
+  el.value = str
+  el.setAttribute('readonly', '')
+  el.style = {position: 'absolute', left: '-9999px'}
+  document.body.appendChild(el)
+  el.select()
+  document.execCommand('copy')
+  document.body.removeChild(el)
+}
+
 export default function () {
   const components = document.querySelectorAll(COMPONENT_SELECTOR)
 
@@ -8,8 +20,11 @@ export default function () {
     const headings = components[i].querySelectorAll(HEADINGS_SELECTOR)
 
     for (let i = 0; i < headings.length; i++) {
-      headings[i].addEventListener('click', (event) => {
-        location.hash = headings[i].getAttribute('id')
+      headings[i].addEventListener('click', () => {
+        history.replaceState({}, '', '#' + headings[i].getAttribute('id'));
+        history.scrollRestoration = 'manual';
+
+        copyStringToClipboard(window.location.href);
       })
     }
   }
