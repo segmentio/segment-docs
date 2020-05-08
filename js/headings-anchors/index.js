6399ab9c9c88c2fc27fde68810d66592763b71b1
Noeemi <klaudiazawodnik@gmail.com>
2020-05-08 10:29:12 +0200

    feat(): Introduce Clipboardjs library

--- js/headings-anchors/index.js
+++ js/headings-anchors/index.js
@@ -1,17 +1,7 @@
+import ClipboardJS from 'clipboard'
+
 const COMPONENT_SELECTOR = '[data-headings-anchors]'
 const HEADINGS_SELECTOR = 'h1, h2, h3, h4, h5, h6'
-
-const copyStringToClipboard = (str) => {
-  const el = document.createElement('textarea')
-
-  el.value = str
-  el.setAttribute('readonly', '')
-  el.style = {position: 'absolute', left: '-9999px'}
-  document.body.appendChild(el)
-  el.select()
-  document.execCommand('copy')
-  document.body.removeChild(el)
-}
 
 export default function () {
   const components = document.querySelectorAll(COMPONENT_SELECTOR)
@@ -20,11 +10,11 @@ export default function () {
     const headings = components[i].querySelectorAll(HEADINGS_SELECTOR)
 
     for (let i = 0; i < headings.length; i++) {
-      headings[i].addEventListener('click', () => {
-        history.replaceState({}, '', '#' + headings[i].getAttribute('id'));
-        history.scrollRestoration = 'manual';
-
-        copyStringToClipboard(window.location.href);
+      new ClipboardJS(headings[i], {
+        text: () => {
+          history.replaceState({}, '', '#' + headings[i].getAttribute('id'))
+          return window.location.href
+        }
       })
     }
   }
