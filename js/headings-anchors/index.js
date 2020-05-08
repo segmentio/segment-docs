65a4d93811e355f959fa1f1bc1a8cf348fd70044
Noeemi <klaudiazawodnik@gmail.com>
2020-05-08 15:34:49 +0200

    feat(copy-to-clipboard): Add tooltip

--- js/headings-anchors/index.js
+++ js/headings-anchors/index.js
@@ -1,7 +1,8 @@
 import ClipboardJS from 'clipboard'
+import tippy from 'tippy.js'
 
 const COMPONENT_SELECTOR = '[data-headings-anchors]'
-const HEADINGS_SELECTOR = 'h1, h2, h3, h4, h5, h6'
+const HEADINGS_SELECTOR = 'h1, h2, h3, h4'
 
 export default function () {
   const components = document.querySelectorAll(COMPONENT_SELECTOR)
@@ -10,11 +11,29 @@ export default function () {
     const headings = components[i].querySelectorAll(HEADINGS_SELECTOR)
 
     for (let i = 0; i < headings.length; i++) {
-      new ClipboardJS(headings[i], {
+      const tooltip = tippy(headings[i], {
+        content: 'Copied!',
+        theme: 'dark',
+        placement: 'top-start',
+        distance: 5,
+        trigger: 'manual'
+      })
+
+      const clipboard = new ClipboardJS(headings[i], {
         text: () => {
           history.replaceState({}, '', '#' + headings[i].getAttribute('id'))
           return window.location.href
         }
+      })
+
+      clipboard.on('success', function(e) {
+        tooltip.show()
+
+        setTimeout(() => {
+          tooltip.hide()
+        }, 700)
+    
+        e.clearSelection()
       })
     }
   }
