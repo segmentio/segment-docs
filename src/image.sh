fb1e7d71256e4fd4b36c6d85264caca550ea561a
markzegarelli <mark.zegarelli@gmail.com>
2020-12-01 15:20:33 -0800

    DOC-82 improve image.sh

--- src/image.sh
+++ src/image.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-cat folders.txt |
+rm _data/img.yml
+echo "---
+image_files:" > _data/img.yml
+
+find . -name '*.md' | sed -E 's|/[^/]+$||' |sort | uniq |
 while read -r loc; do
 echo "  -"
 echo "    folder: $loc";
 echo "    images:"
-grep '\!\[' ${loc}/*.md | sed -E 's/.*(\(.*\))/      - \1/' 
-#ls ${loc}
-done >> img.txt
+grep '\!\[' ${loc}/*.md | sed -E 's/.*\((.*)\)/      - \1/g'
+done >> _data/img.yml
+
+
+
