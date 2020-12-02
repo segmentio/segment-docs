0f93ec8d35fcea399e46e4495b19686d918eb21d
Noeemi <klaudiazawodnik@gmail.com>
2020-12-02 16:50:24 +0100

    wip()

--- js/feedback/index.js
+++ js/feedback/index.js
@@ -1,5 +1,6 @@
 import tippy from 'tippy.js'
 
+const COMPONENT_SELECTOR = '[data-feedback]'
 const BUTTON_SELECTOR = '[data-feedback-button]'
 const SEND_SELECTOR = '[data-send-button]'
 const TEMPLATE_SELECTOR = '[data-feedback-template]'
@@ -9,141 +10,145 @@ const ACTIVE_CLASS = 'data-active-class'
 const typewriter = require('analytics')
 
 export default function() {
-  const buttons = document.querySelectorAll(BUTTON_SELECTOR)
-  const templates = document.querySelectorAll(TEMPLATE_SELECTOR)
-  const helpfulTemplate = [...templates].filter(
-    (template) => template.dataset.feedbackTemplate === 'helpful'
-  )
-  const unhelpfulTemplate = [...templates].filter(
-    (template) => template.dataset.feedbackTemplate === 'unhelpful'
-  )
-  const alternateTemplate = [...templates].filter(
-    (template) => template.dataset.feedbackTemplate === 'alternate'
-  )
-  const helpfulButtons = [...buttons].filter(
-    (button) => button.dataset.feedbackButton === 'helpful'
-  )
-  const unhelpfulButtons = [...buttons].filter(
-    (button) => button.dataset.feedbackButton === 'unhelpful'
-  )
-  const contents = document.querySelectorAll(CONTENT_SELECTOR)
+  const component = document.querySelectorAll(COMPONENT_SELECTOR)
 
-  let sent = false
-  const tooltips = tippy(buttons)
-  const defaultSettings = {
-    interactive: true,
-    placement: 'bottom',
-    boundary: 'viewport',
-    trigger: 'manual',
-    onMount: (tooltip) => {
-      const sendButton = document.querySelector(SEND_SELECTOR)
-      const feedbackText = document.querySelector(TEXT_SELECTOR)
+  for (let i = 0; i < component.length; i++) {
+    const buttons = document.querySelectorAll(BUTTON_SELECTOR)
+    const templates = component[i].querySelectorAll(TEMPLATE_SELECTOR)
+    const helpfulTemplate = [...templates].filter(
+      (template) => template.dataset.feedbackTemplate === 'helpful'
+    )
+    const unhelpfulTemplate = [...templates].filter(
+      (template) => template.dataset.feedbackTemplate === 'unhelpful'
+    )
+    const alternateTemplate = [...templates].filter(
+      (template) => template.dataset.feedbackTemplate === 'alternate'
+    )
+    const helpfulButtons = [...buttons].filter(
+      (button) => button.dataset.feedbackButton === 'helpful'
+    )
+    const unhelpfulButtons = [...buttons].filter(
+      (button) => button.dataset.feedbackButton === 'unhelpful'
+    )
+    const contents = document.querySelectorAll(CONTENT_SELECTOR)
 
-      sendButton.addEventListener('click', () => {
-        const feedbackValue = feedbackText.value
+    let sent = false
+    const tooltips = tippy(buttons)
+    const defaultSettings = {
+      interactive: true,
+      placement: 'bottom',
+      boundary: 'viewport',
+      trigger: 'manual',
+      onMount: (tooltip) => {
+        const sendButton = document.querySelector(SEND_SELECTOR)
+        const feedbackText = component[i].querySelector(TEXT_SELECTOR)
 
-        if (feedbackValue) {
-          let { section = 'footer', helpful = false } = JSON.parse(
-            window.localStorage.getItem('docsFeedback')
-          )
-          typewriter.feedbackCommentProvided({
-            section,
-            helpful,
-            title: `${document.title}`,
-            url: document.url,
-            comment: feedbackValue,
-          })
+        sendButton.addEventListener('click', () => {
+          const feedbackValue = feedbackText.value
 
-          tooltip.hide()
-          sent = true
+          if (feedbackValue) {
+            let { section = 'footer', helpful = false } = JSON.parse(
+              window.localStorage.getItem('docsFeedback')
+            )
+            typewriter.feedbackCommentProvided({
+              section,
+              helpful,
+              title: `${document.title}`,
+              url: document.url,
+              comment: feedbackValue,
+            })
 
-          for (let i = 0; i < contents.length; i++) {
+            tooltip.hide()
+            sent = true
+
+            for (let i = 0; i < contents.length; i++) {
+              contents[i].hidden = false
+              contents[i].children[0].hidden = false
+              contents[i].children[1].hidden = true
+            }
+          }
+        })
+      },
+      onShown: () => {
+        const feedbackText = document.querySelector(TEXT_SELECTOR)
+        feedbackText.focus()
+      },
+      onHidden: () => {
+        if (!sent) {
+          for (let i = 0; i < contents.length; i += 1) {
             contents[i].hidden = false
-            contents[i].children[0].hidden = false
-            contents[i].children[1].hidden = true
+            contents[i].children[0].hidden = true
           }
         }
+      },
+    }
+
+    const trackFeedback = (helpful, section) => {
+      typewriter.feedbackProvided({
+        title: `${document.title}`,
+        helpful,
+        url: document.url,
+        section,
       })
-    },
-    onShown: () => {
-      const feedbackText = document.querySelector(TEXT_SELECTOR)
-      feedbackText.focus()
-    },
-    onHidden: () => {
-      if (!sent) {
-        for (let i = 0; i < contents.length; i += 1) {
-          contents[i].hidden = false
-          contents[i].children[0].hidden = true
+    }
+
+    const clickHandler = () => {
+      for (let i = 0; i < buttons.length; i++) {
+        buttons[i].disabled = true
+      }
+    }
+
+    const addActiveClasses = (elements) => {
+      for (let i = 0; i < elements.length; i++) {
+        const activeClasses = elements[i].getAttribute(ACTIVE_CLASS).split(' ')
+        
+        for (let c = 0; c < activeClasses.length; c++) {
+          elements[i].classList.add(activeClasses[c])
         }
       }
-    },
-  }
+    }
 
-  const trackFeedback = (helpful, section) => {
-    typewriter.feedbackProvided({
-      title: `${document.title}`,
-      helpful,
-      url: document.url,
-      section,
-    })
-  }
+    for (let i = 0; i < tooltips.length; i++) {
+      tooltips[i].setProps(defaultSettings)
+    }
 
-  const clickHandler = () => {
     for (let i = 0; i < buttons.length; i++) {
-      buttons[i].disabled = true
+      buttons[i].addEventListener('click', (event) => {
+        event.preventDefault()
+
+        const section = buttons[i].hasAttribute('data-section')
+          ? 'right-nav'
+          : 'footer'
+        const helpful = buttons[i].dataset.feedbackButton === 'helpful'
+        const alternate = buttons[i].dataset.feedbackButton === 'alternate'
+
+        window.localStorage.setItem(
+          'docsFeedback',
+          JSON.stringify({
+            section,
+            helpful,
+          })
+        )
+
+        if (alternate) {
+          tooltips[i].setContent(alternateTemplate[0].innerHTML)
+        }
+
+        if (helpful & !alternate) {
+          addActiveClasses(helpfulButtons)
+          trackFeedback(true, section)
+
+          tooltips[i].setContent(helpfulTemplate[0].innerHTML)
+        } else if (!helpful & !alternate) {
+          addActiveClasses(unhelpfulButtons)
+          trackFeedback(false, section)
+
+          tooltips[i].setContent(unhelpfulTemplate[0].innerHTML)
+        }
+
+        tooltips[i].show()
+        clickHandler()
+      })
     }
-  }
-
-  const addActiveClasses = (elements) => {
-    for (let i = 0; i < elements.length; i++) {
-      const activeClasses = elements[i].getAttribute(ACTIVE_CLASS).split(' ')
-      
-      for (let c = 0; c < activeClasses.length; c++) {
-        elements[i].classList.add(activeClasses[c])
-      }
-    }
-  }
-
-  for (let i = 0; i < tooltips.length; i++) {
-    tooltips[i].setProps(defaultSettings)
-  }
-
-  for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', (event) => {
-      event.preventDefault()
-
-      const section = buttons[i].hasAttribute('data-section')
-        ? 'right-nav'
-        : 'footer'
-      const helpful = buttons[i].dataset.feedbackButton === 'helpful'
-      const alternate = buttons[i].dataset.feedbackButton === 'alternate'
-
-      window.localStorage.setItem(
-        'docsFeedback',
-        JSON.stringify({
-          section,
-          helpful,
-        })
-      )
-
-      if (alternate) {
-        tooltips[i].setContent(alternateTemplate[0].innerHTML)
-      }
-
-      if (helpful & !alternate) {
-        addActiveClasses(helpfulButtons)
-        trackFeedback(true, section)
-
-        tooltips[i].setContent(helpfulTemplate[0].innerHTML)
-      } else if (!helpful & !alternate) {
-        addActiveClasses(unhelpfulButtons)
-        trackFeedback(false, section)
-
-        tooltips[i].setContent(unhelpfulTemplate[0].innerHTML)
-      }
-
-      tooltips[i].show()
-      clickHandler()
-    })
   }
 }
