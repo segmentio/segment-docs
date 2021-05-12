345741135499e0d3b8d67c72724a46cf89c8ea12
markzegarelli <mark.zegarelli@gmail.com>
2021-05-12 14:21:32 -0700

    testing that [netlify-ignore] still works

--- scripts/ignore.sh
+++ scripts/ignore.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
+# grab the latest entry from git log
 log=$(env -i git log -1 --pretty=%B)
 
+# run a series of checks for common reasons that we shouldn't preview build.
 if echo "$log" | grep -q 'typo'; then
 exit
 elif echo "$log" | grep -q 'link'; then
 exit
-#elif echo "$log" | grep -q '\\[netlify\\-ignore\\]'; then
-#exit
+elif echo "$log" | grep -q '\\[netlify\\-ignore\\]'; then
+exit
 fi
-#echo $log
