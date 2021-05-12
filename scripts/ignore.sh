e5974c7695e967ffd41bfcc3074c5284f13c0be6
markzegarelli <mark.zegarelli@gmail.com>
2021-05-12 14:46:31 -0700

    once more [netlify-ignore]

--- scripts/ignore.sh
+++ scripts/ignore.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # grab the latest entry from git log
-log=$(env -i git log -1 --pretty=%B)
-
+log=$(env -i cd .. && git log -1 --pretty=%B)
+echo $log
 # run a series of checks for common reasons that we shouldn't preview build.
 # if no checks match, exit with code 1 to run the build
 
@@ -13,7 +13,7 @@ exit
 elif echo "$log" | grep -q 'link'; then
 exit
 # ignore when the ignore tag is explicitly added
-elif echo "$log" | grep -q '\\[netlify\\-ignore\\]'; then
+elif echo "$log" | grep -q 'netlify\-ignore'; then
 exit 
 # ignore dependabot updates
 elif echo "$log" | grep -q 'dependabot'; then
@@ -24,4 +24,4 @@ exit
 else
 exit 1
 fi
-echo $log
+
