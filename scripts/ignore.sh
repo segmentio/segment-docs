45a8d04409d876ee511d3250ba2d9251fd3926eb
markzegarelli <mark.zegarelli@gmail.com>
2021-05-12 14:29:00 -0700

    Merge branch 'Master' into

--- scripts/ignore.sh
+++ scripts/ignore.sh
@@ -4,10 +4,18 @@
 log=$(env -i git log -1 --pretty=%B)
 
 # run a series of checks for common reasons that we shouldn't preview build.
+# if no checks match, exit with code 1 to run the build
 if echo "$log" | grep -q 'typo'; then
 exit
 elif echo "$log" | grep -q 'link'; then
 exit
 elif echo "$log" | grep -q '\\[netlify\\-ignore\\]'; then
+exit 
+elif echo "$log" | grep -q 'dependabot'; then
 exit
+elif echo "$log" | grep -q 'Merge branch \\SMaster\\S'; then
+exit
+else
+exit 1
 fi
+echo $log
