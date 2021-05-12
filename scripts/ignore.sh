e1097626658c554cd71e2d26c7f4921f78187c5e
markzegarelli <mark.zegarelli@gmail.com>
2021-05-12 14:31:14 -0700

    update toml with ignore script [netlify-ignore]

--- scripts/ignore.sh
+++ scripts/ignore.sh
@@ -5,15 +5,21 @@ log=$(env -i git log -1 --pretty=%B)
 
 # run a series of checks for common reasons that we shouldn't preview build.
 # if no checks match, exit with code 1 to run the build
+
+# ignore for simple typo fixes
 if echo "$log" | grep -q 'typo'; then
 exit
+# ignore link / url updates
 elif echo "$log" | grep -q 'link'; then
 exit
+# ignore when the ignore tag is explicitly added
 elif echo "$log" | grep -q '\\[netlify\\-ignore\\]'; then
 exit 
+# ignore dependabot updates
 elif echo "$log" | grep -q 'dependabot'; then
 exit
-elif echo "$log" | grep -q 'Merge branch \\SMaster\\S'; then
+# ignore when master is merged into a branch
+elif echo "$log" | grep -q 'Merge branch \SMaster\S'; then
 exit
 else
 exit 1
