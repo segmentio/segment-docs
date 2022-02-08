22f459b8a838c38bdd7c8e8b72299370a052e2d1
markzegarelli <mark.zegarelli@segment.com>
2022-02-08 09:06:44 -0800

    Make Sprig Web (Actions) documentation visbile (#2466)

--- scripts/catalog_papi.js
+++ scripts/catalog_papi.js
@@ -330,7 +330,7 @@ const updateDestinations = async () => {
     let regional = ['us-west']
 
     // We need to be able to keep the system slug in some cases.
-    const slugOverrides = ['actions-google-enhanced-conversions', 'actions-google-analytics-4', 'actions-facebook-conversions-api',  'actions-friendbuy-cloud']
+    const slugOverrides = ['actions-google-enhanced-conversions', 'actions-google-analytics-4', 'actions-facebook-conversions-api',  'actions-friendbuy-cloud', 'sprig-web']
     let slug = slugify(destination.name)
     if (slugOverrides.includes(destination.slug)) {
       slug = destination.slug
